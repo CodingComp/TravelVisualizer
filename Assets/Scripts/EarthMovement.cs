486b43ef4f751bef5997ff3b4052f36774ac8723
CodingComp <joshtcgdaniels@gmail.com>
2026-01-16 14:32:49 -0700

    Initial project

---
+++ Assets/Scripts/EarthMovement.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+public class EarthMovement : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = -1f; // Degrees per second
+    
+    void Update()
+    {
+        // Rotate around the y-axis
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    }
+}
